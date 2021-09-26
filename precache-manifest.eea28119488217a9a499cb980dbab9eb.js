db19d6c2a683a0f6d5c358bceafc972b30eb7bae
Abhishek Narkhede <abhisheknarkhede123@gmail.com>
2021-09-26 17:41:59 +0530

    Updates

---
+++ precache-manifest.eea28119488217a9a499cb980dbab9eb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77df0c8bef806a4dfa8e1e82a38d5764",
+    "url": "/keeper_app/index.html"
+  },
+  {
+    "revision": "25f9368d4d674f4ed492",
+    "url": "/keeper_app/static/js/2.72aa472d.chunk.js"
+  },
+  {
+    "revision": "1de99188410dd4ee6ec9",
+    "url": "/keeper_app/static/js/main.bc009953.chunk.js"
+  },
+  {
+    "revision": "7b57fd686ca7e72bb1bb",
+    "url": "/keeper_app/static/js/runtime-main.c42c0acf.js"
+  }
+]);
