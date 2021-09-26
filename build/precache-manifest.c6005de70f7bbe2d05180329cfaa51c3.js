2fab831d97c7e6202ced639c8e03be27a8219abf
Abhishek Narkhede <abhisheknarkhede123@gmail.com>
2021-09-26 16:28:50 +0530

    first commit keeper

---
+++ build/precache-manifest.c6005de70f7bbe2d05180329cfaa51c3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be594abb1dbb8c5364d4c024b3f57b10",
+    "url": "/keeper_app/index.html"
+  },
+  {
+    "revision": "25f9368d4d674f4ed492",
+    "url": "/keeper_app/static/js/2.72aa472d.chunk.js"
+  },
+  {
+    "revision": "1de99188410dd4ee6ec9",
+    "url": "/keeper_app/static/js/main.bc009953.chunk.js"
+  },
+  {
+    "revision": "7b57fd686ca7e72bb1bb",
+    "url": "/keeper_app/static/js/runtime-main.c42c0acf.js"
+  }
+]);
